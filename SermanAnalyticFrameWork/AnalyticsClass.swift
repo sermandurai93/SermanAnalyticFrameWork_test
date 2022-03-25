2c6e3fc1063f0b041655ba312ebe3de9fbc8ce55
Sermandurai.S <sermandurai.s@redbus.com>
2022-03-25 19:01:35 +0530

    Initial commit

---
+++ SermanAnalyticFrameWork/AnalyticsClass.swift
@@ -0,0 +1,14 @@
+//
+//  AnalyticsClass.swift
+//  SermanAnalyticFrameWork
+//
+//  Created by Sermandurai Subbaiya on 25/03/22.
+//
+
+import Foundation
+
+class AnalytisClass {
+    func printMessage() {
+        print("Success")
+    }
+}
---
+++ SermanAnalyticFrameWork/SermanAnalyticFrameWork.h
@@ -0,0 +1,18 @@
+//
+//  SermanAnalyticFrameWork.h
+//  SermanAnalyticFrameWork
+//
+//  Created by Sermandurai Subbaiya on 25/03/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SermanAnalyticFrameWork.
+FOUNDATION_EXPORT double SermanAnalyticFrameWorkVersionNumber;
+
+//! Project version string for SermanAnalyticFrameWork.
+FOUNDATION_EXPORT const unsigned char SermanAnalyticFrameWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SermanAnalyticFrameWork/PublicHeader.h>
+
+
